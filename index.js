cd26f88b472fbc2088da1b537187319e900f43fa
HASSAN <80027564+0hassan@users.noreply.github.com>
2022-08-12 00:02:15 +0400

    v1

--- index.js
+++ index.js
@@ -27,47 +27,34 @@ app.get("/api/1451001600000", function (req, res) {
   res.json({ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
 });
 
-// date validator
-var date_validator = function (testDate) {
-  try {
-    var num = Number(testDate);
-    date = new Date(num);
-  } catch (any) {
-    var str = testDate;
-    date = new Date(str);
-  }
-  
-  var valid = (date).getTime() >= 0;
-  return valid;
-}
-
 // utc date setter
 var utcDate = function (testDate) {
-  var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
   const date = new Date(testDate);
-
-  return days[date.getDay()] + ", " + date.getDate() + " " + months[date.getMonth()] + " " + date.getUTCFullYear() + " 00:00:00 GMT";
+  return date.toUTCString();
 }
 
+var response = {};
 //parsing date into timestamp 
 app.get("/api/:date", function (req, res) {
-    var dt = req.params.date;
+  var dt = req.params.date;
 
-  //var testedDate = date_validator(dt);
-  if (dt != null) {
-    if(dt.includes('-')){
-      res.json({ unix: Date.parse(dt), utc: utcDate(new Date(dt)) });
-    }
-    
+  if (dt.includes('-')) {
+    response['unix'] = Date.parse(dt).toString();
+    response['utc'] = utcDate(new Date(dt)).toString();
+
   }
-  else {
-     res.json({ unix: dt, utc: utcDate(new Date(dt) )});
-   }
+  if (!isNaN(dt)) {
+    response['unix'] = dt.toString();
+    response['utc'] = utcDate(new Date(Number(dt))).toString();
+
+  }
+  if ((response['unix'].valueOf() === 'NaN') || (response['utc'].valueOf() === "Invalid Date")) {
+    res.json({ error: "Invalid Date" });
+  }
+  res.json(response);
 });
 
 
-// listen for requests :)
 var listener = app.listen(3500, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
