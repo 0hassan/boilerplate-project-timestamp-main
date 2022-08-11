34cfac6e0f3baf28bc15a59a5fa0fc82a9468744
HASSAN <80027564+0hassan@users.noreply.github.com>
2022-08-11 14:50:14 +0400

    v1

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -16,42 +16,46 @@ app.use(express.static('public'));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
-}); 
+});
 
 
 // your first API endpoint... 
 app.get("/api/1451001600000", function (req, res) {
-  res.json({ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT"});
+  res.json({ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
 });
 
 // date validator
-var date_validator = function(testDate){
-  var valid = (new Date(testDate)).getTime() > 0;
+var date_validator = function (testDate) {
+  date = new Date(Number(testDate));
+  var valid = (date).getTime() >= 0;
   return valid;
 }
 
 // utc date setter
-var utcDate = function(testDate){
+var utcDate = function (testDate) {
   var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
   const date = new Date(testDate);
-  
-  return days[date.getDay()]+", "+date.getDate()+ " "+months[date.getMonth()]+ " "+date.getUTCFullYear() + " 00:00:00 GMT";
+
+  return days[date.getDay()] + ", " + date.getDate() + " " + months[date.getMonth()] + " " + date.getUTCFullYear() + " 00:00:00 GMT";
 }
 
 //parsing date into timestamp 
 app.get("/api/:date?", function (req, res) {
-
-  var dt = req.query.date.toString();
-  var testedDate = date_validator(dt) ;
-  if(dt === ""){
-    res.json({ unix: Date.now().toString() , utc: utcDate(new Date()) });
+  try {
+    var dt = req.query.date.toString();
+  } catch (error) {
+    console.error('No Input Has Given');
   }
-  else if(testedDate){
-    const dt = Date.parse(testDate); 
-    res.json({ unix: dt, utc: utcDate(testDate) });
-  }else{
-    res.json({ error : "Invalid Date" });
+
+  var testedDate = date_validator(dt);
+  if (dt === "") {
+    res.json({ unix: Date.now().toString(), utc: utcDate(new Date()) });
+  }
+  else if (testedDate) {
+    res.json({ unix: dt, utc: utcDate(new Date(Number(dt))) });
+  } else {
+    res.json({ error: "Invalid Date" });
   }
 });
 
